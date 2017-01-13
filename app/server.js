e446d14b765577a03e62afca7e32556269ed8c18
Traceron <fbflat45@gmail.com>
2017-01-13 16:32:09 -0500

    Clean up static asset routing.

--- app/server.js
+++ app/server.js
@@ -10,13 +10,10 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.use('/js', express.static(path.resolve(root, 'js'), { index: false }));
-app.use('/css', express.static(path.resolve(root, 'css'), { index: false }));
-app.use('/images', express.static(path.resolve(root, 'images'), { index: false }));
-
-app.get('/js*', (req, res) => res.sendStatus(404));
-app.get('/css*', (req, res) => res.sendStatus(404));
-app.get('/images*', (req, res) => res.sendStatus(404));
+[ 'js', 'css', 'images' ].forEach((route) => {
+    app.use('/' + route, express.static(path.resolve(root, route), { index: false }));
+    app.get('/' + route + '*', (req, res) => res.sendStatus(404));
+});
 
 app.get('*', (req, res) => {
     res.sendFile('index.html', { root });
