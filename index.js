a5f446746acdff92dfd1684840135d8a90a8ff86
Traceron <fbflat45@gmail.com>
2017-01-13 12:49:49 -0500

    Clean up server.

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.set('port', (process.env.PORT || 80));
-
 app.get('/', function (req, res) {
     res.send('Hello Cubers!');
 });
 
-app.listen(app.get('port'), function () {
-    console.log('Example app listening on port ' + app.get('port') + '!');
-});
+app.listen(process.env.PORT || 80);
