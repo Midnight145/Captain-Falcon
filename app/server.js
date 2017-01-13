b65d1eac262ae9fad112c217f12f8b23da393eff
Traceron <fbflat45@gmail.com>
2017-01-13 15:34:59 -0500

    Fixed production issues.

--- app/server.js
+++ app/server.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const dev = process.env.NODE_ENV === 'development';
 
+const root = dev ? './dev' : './prod';
+
 const app = express();
 
-app.use('/js', express.static('dev/js'));
+app.use('/js', express.static(path.resolve(root, 'js')));
 
 app.get('*', function (req, res) {
-    res.sendFile('index.html', { root: path.resolve('./dev') });
+    res.sendFile('index.html', { root });
 });
 
 app.listen(process.env.PORT || 80);
