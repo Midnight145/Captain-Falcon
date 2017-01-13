7d6ada18bf506b26b98f71516ec71185d841457a
Traceron <fbflat45@gmail.com>
2017-01-13 18:52:46 -0500

    Fix production config and build files.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     entry: './app/index.jsx',
     output: {
-        filename: 'client.js',
+        filename: 'client.min.js',
         path: './prod/js',
         publicPath: '/js/'
     },
