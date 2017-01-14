5381c07a5a3b56465f8fc3dabcafa982c0e7da64
Traceron <fbflat45@gmail.com>
2017-01-13 19:37:05 -0500

    Add random COLL case and update production build.

--- app/components/cube/index.jsx
+++ app/components/cube/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export class Cube extends React.Component {
     render() {
-        return <div>I am a cube!</div>;
+        const { size, stage, alg } = this.props;
+        return <div>
+            <img src={'http://cube.crider.co.uk/visualcube.png?fmt=svg&bg=t&stage=' + stage + '&size=' + size + '&case=' + alg} />
+        </div>;
     }
 }
--- app/components/training/index.jsx
+++ app/components/training/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { IndexLink } from 'react-router';
 
+import { Cube } from '../cube';
+
 export class Training extends React.Component {
     render() {
+        const collT = [
+            "R U2' R' U' R U' R2 U2' R U R' U R",
+            "R' U R U2' R' L' U R U' L",
+            "y l' U' L U R U' r' F",
+            "y2 F R U R' U' R U' R' U' R U R' F'",
+            "y' r U R' U' r' F R F'",
+            "R' U R2 D r' U2 r D' R2 U' R"
+        ];
+        const size = 200;
+        const stage = 'coll';
+        const alg = collT[Math.floor(Math.random()*collT.length)].replace(' ', '');
         return <div>
             <IndexLink to='/'>Home</IndexLink>
             <div>This is the training page!</div>
+            <Cube size={size} stage={stage} alg={alg}/>
         </div>;
     }
 }
